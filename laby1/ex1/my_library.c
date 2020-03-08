acbc6247bde0ee8cf94d28de0eba5d3a15f996f3
Witold Serwatka <witold.serwatka@pm.me>
2020-03-08 17:29:19 +0100

    added structure creation methods

--- laby1/ex1/my_library.c
+++ laby1/ex1/my_library.c
@@ -1,11 +1,25 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+#include "my_library.h"
 
-struct block {
-    char *edit_op;
-};
+int size = 140;
 
-struct main_array {
+struct Main_array create_array(int size){
+    struct Main_array array;
+
+    array.blocks = (struct Block*) calloc(size, sizeof(struct Block));
+    array.size = 0;
+
+    return array;
+}
+
+struct File_pair create_file_pair(FILE first, FILE second){
+    struct File_pair file_pair;
+
+    file_pair.first = first;
+    file_pair.second = second;
+
+    return file_pair;
+}
+
+char* compare_files(struct File_pair pair){
     
-};
+}
