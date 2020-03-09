4c18949d1caa30684b32379f9e5193b4fbd4e549
Witold Serwatka <witold.serwatka@pm.me>
2020-03-09 16:56:18 +0100

    added compare_pairs command

--- laby1/ex1/main.c
+++ laby1/ex1/main.c
@@ -15,18 +15,62 @@ bool isNumber(char* var){
     return true;
 }
 
+bool isCommand(char* str){
+    return strcmp(str, "create_table") == 0
+        || strcmp(str, "compare_pairs") == 0
+        || strcmp(str, "remove_block") == 0
+        || strcmp(str, "remove_operation") == 0;
+}
+
 int main(int argc, char** argv){
     if(argc < 2){
         error("Invalid number of arguments");
     }
 
+    struct Main_array array;
+
     for(int i = 1; i < argc; i++){
         if(strcpm(argv[i], "create_table") == 0){
-            if()
+            if(!isNumber(argv[i+1])){
+                error("Invalid argument: should be of type int");
+            }
+
+            printf("Creating table of size %d\n", argv[i+1]);
+
+            array = create_array(argv[i+1]);
+            i += 2;
         }
         else if(strcpm(argv[i], "compare_pairs") == 0)
         {
-            
+            while(!isCommand(argv[++i])){
+                char* input = calloc(strlen(argv[i]), sizeof(char));
+                char* file1, file2;
+                int name_length = 0;
+
+                for(int j = 0; j < strlen(argv[i]); j++){
+                    if(argv[i][j] == ':'){
+                        file1 = calloc(j-1, sizeof(char));
+                        file2 = calloc(strlen(argv[i]) - j, sizeof(char));
+
+                        strcpy(file1, input);
+                        strcpy(input, "");
+                        name_length = 0;
+                    }
+                    else{
+                        input[name_length++] = argv[j];
+                    }
+                }
+
+                strcpy(file2, input);
+                struct File_pair pair = create_file_pair(file1, file2);
+                
+                free(input);
+                free(file1);
+                free(file2);
+
+                char* tmp_filename = compare_files(pair);
+                printf("Created block number %d", create_block(array, tmp_filename));
+            }
         }
         else if(strcpm(argv[i], "remove_block") == 0)
         {
