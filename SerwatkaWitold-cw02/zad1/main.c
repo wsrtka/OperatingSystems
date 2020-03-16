67d2ee26f030c61b9f3005c1109a009a99d954b1
wsrtka <witold.serwatka@pm.me>
2020-03-16 13:16:50 +0100

    added sort command

--- SerwatkaWitold-cw02/zad1/main.c
+++ SerwatkaWitold-cw02/zad1/main.c
@@ -29,28 +29,62 @@ int main(int argc, char** argv){
     for(int i = 1; i < argc; i++){
         if(strcmp(argv[i++], "generate") == 0){
             if(is_number(argv[i]) != -1){
-                error("'generate' 1st argument should be string");
+                error("'generate' usage: generate <file name> <records> <bytes>");
             }
             char* filename = calloc(strlen(argv[i]), sizeof(char));
             strcpy(filename, argv[i++]);
 
             if(is_number(argv[i]) == -1){
-                error("'generate' 2nd argument should be number");
+                error("'generate' usage: generate <file name> <records> <bytes>");
             }
             int lines_count = is_number(argv[i++]);
 
             if(is_number(argv[i]) == -1){
-                error("'generate' 3rd argument should be number");
+                error("'generate' usage: generate <file name> <records> <bytes>");
             }
             int bytes = is_number(argv[i++]);
 
             generate_file(filename, lines_count, bytes);
         }
-        else if(strcmp(argv[i], "sort") == 0){
+        else if(strcmp(argv[i++], "sort") == 0){
+            if(is_number(argv[i]) != -1){
+                error("'sort' usage: sort <file name> <records> <bytes> <lib | sys>");
+            }
+            char* filename = calloc(strlen(argv[i]), sizeof(char));
+            strcpy(filename, argv[i++]);
 
+            if(is_number(argv[i]) == -1){
+                error("'sort' usage: sort <file name> <records> <bytes> <lib | sys>");
+            }
+            int records = is_number(argv[i++]);
+
+            if(is_number(argv[i]) == -1){
+                error("'sort' usage: sort <file name> <records> <bytes> <lib | sys>");
+            }
+            int bytes = is_number(argv[i++]);
+
+            if(is_number(argv[i]) != -1){
+                error("'sort' usage: sort <file name> <records> <bytes> <lib | sys>");
+            }
+            
+            if(strcmp(argv[i], "sys") == 0){
+                int fd = open(filename, O_RDWR);
+
+                sys_qsort(fd, 0, records, bytes);
+            }
+            else if(strcmp(argv[i], "lib") == 0){
+                FILE* file = fopen(filename, "a+");
+
+                lib_qsort(file, 0, records, bytes);
+            }
+            else{
+                error("'sort' usage: sort <file name> <records> <bytes> <lib | sys>");
+            }
+
+            i++;
         }
-        else if(strcmp(argv[i], "copy") == 0){
-
+        else if(strcmp(argv[i++], "copy") == 0){
+            if(is_number())
         }else{
             error("Could not understand argument");
         }
--- SerwatkaWitold-cw02/zad1/myfslib.h
+++ SerwatkaWitold-cw02/zad1/myfslib.h
@@ -4,7 +4,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-// #include <stdbool.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/types.h>
