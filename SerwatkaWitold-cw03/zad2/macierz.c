9817ee45495c540fe3121d22f4e39a22ac1a921a
wsrtka <witold.serwatka@pm.me>
2020-03-22 11:40:13 +0100

    matrix reading

--- SerwatkaWitold-cw03/zad2/macierz.c
+++ SerwatkaWitold-cw03/zad2/macierz.c
@@ -5,10 +5,20 @@
 #include <string.h>
 #include <ctype.h>
 
+struct matrix_dim{
+    int width;
+    int height;
+};
+
+struct matrix{
+    short* values;
+    struct matrix_dim dim;
+};
+
 //===================HELPER FUNCTIONS================//
 
 void error(int exit_code){
-    printf("Program usage: ./macierz <source file> <nr of child processes> <time>\n");
+    printf("Program usage: ./macierz <source file> <nr of child processes> <time> <common|separate>\n");
     exit(exit_code);
 }
 
@@ -21,10 +31,49 @@ int is_number(char* str){
     return 1;
 }
 
+struct matrix_dim get_matrix_size(FILE* file){
+    int width = 0, height = 1;
+    char c;
+    do{
+        c = (char)fgetc(file);
+        if(c == ',' && height == 1){
+            width++;
+        }
+        if(c == '\n'){
+            height++;
+        }
+    }while(c != EOF);
+
+    struct matrix_dim result;
+    result.height = height;
+    result.width = width;
+
+    return result;
+}
+
+struct matrix read_matrix(char* file_name){
+    FILE* file = fopen(file_name, "r");
+    if(file == NULL){
+        error(EXIT_FAILURE);
+    }
+
+    struct matrix_dim dim = get_matrix_size(file);
+    struct matrix result;
+
+    result.dim = dim;
+    result.values = calloc(result.dim.width * result.dim.height, sizeof(short));
+
+    fclose(file);
+
+    printf("%d\n%d\n", result.dim.height, result.dim.width);
+
+    return result;
+}
+
 //========================MAIN=======================//
 
 int main(int argc, char** argv){
-    if(argc != 4 || is_number(argv[1]) == 1 || is_number(argv[2]) == 0 || is_number(argv[3]) == 0){
+    if(argc != 5 || is_number(argv[1]) == 1 || is_number(argv[2]) == 0 || is_number(argv[3]) == 0 || is_number(argv[4]) == 1){
         error(EXIT_FAILURE);
     }
 
@@ -32,6 +81,8 @@ int main(int argc, char** argv){
 
     int child_processes = atoi(argv[2]);
     int max_time = atoi(argv[3]);
+    int common_flag;
+    strcmp(argv[4], "common") == 0 ? common_flag = 1 : strcmp(argv[4], "separate") == 0 ? common_flag = 0 : error(EXIT_FAILURE);
 
     FILE* source_file = fopen(source_file_name, "r");
     if(source_file == NULL){
@@ -44,7 +95,7 @@ int main(int argc, char** argv){
     fscanf(source_file, "%s %s %s", A_matrix_file_name, B_matrix_file_name, C_matrix_file_name);
     fclose(source_file);
 
-    printf("%d\n%d", child_processes, max_time);
+    struct matrix A_matrix = read_matrix(A_matrix_file_name);
 
     return EXIT_SUCCESS;
 }
