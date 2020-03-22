60025650d201b857c6cd536a4e6d5183e133fef3
wsrtka <witold.serwatka@pm.me>
2020-03-22 10:37:58 +0100

    reading input

--- SerwatkaWitold-cw03/zad2/macierz.c
+++ SerwatkaWitold-cw03/zad2/macierz.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <string.h>
+#include <ctype.h>
 
 //===================HELPER FUNCTIONS================//
 
@@ -10,10 +12,39 @@ void error(int exit_code){
     exit(exit_code);
 }
 
+int is_number(char* str){
+    for(int i = 0; i < strlen(str); i++){
+        if(isdigit(str[i]) == 0){
+            return 0;
+        }
+    }
+    return 1;
+}
+
+//========================MAIN=======================//
+
 int main(int argc, char** argv){
-    if(argc != 4){
+    if(argc != 4 || is_number(argv[1]) == 1 || is_number(argv[2]) == 0 || is_number(argv[3]) == 0){
         error(EXIT_FAILURE);
     }
 
-    return 0;
+    char* source_file_name = argv[1];
+
+    int child_processes = atoi(argv[2]);
+    int max_time = atoi(argv[3]);
+
+    FILE* source_file = fopen(source_file_name, "r");
+    if(source_file == NULL){
+        error(EXIT_FAILURE);
+    }
+
+    char* A_matrix_file_name[1024], B_matrix_file_name[1024], C_matrix_file_name[1024];
+
+    rewind(source_file);
+    fscanf(source_file, "%s %s %s", A_matrix_file_name, B_matrix_file_name, C_matrix_file_name);
+    fclose(source_file);
+
+    printf("%d\n%d", child_processes, max_time);
+
+    return EXIT_SUCCESS;
 }
