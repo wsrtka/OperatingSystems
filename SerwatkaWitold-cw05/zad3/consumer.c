0c3eb5169050823790cc5962358bf9bb167c1dbc
wsrtka <witold.serwatka@pm.me>
2020-04-09 12:16:29 +0200

    added reading and writing

--- SerwatkaWitold-cw05/zad3/consumer.c
+++ SerwatkaWitold-cw05/zad3/consumer.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 int main(int argc, char** argv){
     if(argc != 4){
@@ -7,5 +9,33 @@ int main(int argc, char** argv){
         exit(EXIT_FAILURE);
     }
 
+    char* pipe = argv[1];
+    char* dest_filename = argv[2];
+    int n = atoi(argv[3]);
+
+    int fd_pipe = open(pipe, O_RDONLY);
+    if(fd_pipe < 0){
+        print("Ubnable to open pipe.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int fd_dest = open(dest_filename, O_WRONLY);
+    if(fd_dest < 0){
+        print("Unable to open destination file.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    lseek(fd_pipe, 0, SEEK_SET);
+    lseek(fd_dest, 0, SEEK_SET);
+
+    char* buffer = calloc(n, sizeof(char));
     
+    while(read(fd_pipe, &buffer, n) != 0){
+        if(write(fd_dest, &buffer, n) == 0){
+            print("Unable to write to destination file.\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    return 0;
 }
