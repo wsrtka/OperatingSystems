122a8fb2372b4ec9a789830fe6273d61ca55c459
wsrtka <witold.serwatka@pm.me>
2020-04-20 10:45:50 +0200

    tuned server initialization

---
+++ SerwatkaWitold-cw06/zad1/client.c
@@ -0,0 +1,11 @@
+#include "header.h"
+#include <sys/msg.h>
+#include <sys/ipc.h>
+#include <stdlib.h>
+#include <signal.h>
+
+int main(){
+    printf("Initializing client...\n");
+
+    return 0;
+}
--- SerwatkaWitold-cw06/zad1/server.c
+++ SerwatkaWitold-cw06/zad1/server.c
@@ -11,18 +11,37 @@ void sigint_handler(){
 }
 
 int main(){
+    printf("Initializing server...\n");
+
     signal(SIGINT, sigint_handler);
 
-    char file_path[PATHLENGTH];
-    realpath("server.c", file_path);
-    key_t obj_key = ftok(file_path, PROJECT);
+    printf("Signal handler set.\n");
+
+    char* file_path;   
+    if((file_path = getenv("HOME")) == -1){
+        printf("Error getting file path.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    key_t obj_key;
+    if((obj_key = ftok(file_path, PROJECT)) == -1){
+        printf("Error getting object key.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Object key aquired.\n");
 
     if((queue_id = msgget(obj_key, IPC_CREAT)) == -1){
         printf("Error encountered in queue creation.\n");
         exit(EXIT_FAILURE);
     }
     
-    
+    printf("Initialized new message queue with id %d\n", queue_id);
+    printf("Server initialized.\n");
+
+    while(1){
+        //obsługa komunikatów
+    }
 
     return 0;
 }
