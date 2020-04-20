3a9d77954d0c1d0d0269b670a91a4624c0727741
wsrtka <witold.serwatka@pm.me>
2020-04-20 10:11:53 +0200

    basic server init

--- SerwatkaWitold-cw06/zad1/header.h
+++ SerwatkaWitold-cw06/zad1/header.h
@@ -1,11 +1,13 @@
-#ifndef settings
-#define settings
+#ifndef header
+#define header
 
-#define MSG_SIZE 4096;
-#define STOP  0;
-#define DISCONNECT 1;
-#define LIST 2;
-#define CONNECT 3;
-#define PROJECT 'c';
+#define MSG_SIZE 4096
+#define PATHLENGTH 256
+#define MAXCLIENTS 32
+#define STOP  0
+#define DISCONNECT 1
+#define LIST 2
+#define CONNECT 3
+#define PROJECT 'c'
 
 #endif
--- SerwatkaWitold-cw06/zad1/server.c
+++ SerwatkaWitold-cw06/zad1/server.c
@@ -1,1 +1,28 @@
+#include "header.h"
+#include <sys/msg.h>
+#include <sys/ipc.h>
+#include <stdlib.h>
+#include <signal.h>
 
+int queue_id;
+
+void sigint_handler(){
+
+}
+
+int main(){
+    signal(SIGINT, sigint_handler);
+
+    char file_path[PATHLENGTH];
+    realpath("server.c", file_path);
+    key_t obj_key = ftok(file_path, PROJECT);
+
+    if((queue_id = msgget(obj_key, IPC_CREAT)) == -1){
+        printf("Error encountered in queue creation.\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    
+
+    return 0;
+}
