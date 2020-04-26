5084565d7ae1502072c93f1805e393a3dff80a10
wsrtka <witold.serwatka@pm.me>
2020-04-26 13:59:04 +0200

    exersize 7 setup

---
+++ SerwatkaWitold-cw07/zad1/settings.h
@@ -0,0 +1,15 @@
+#ifndef settings
+#define settings
+
+#include <errno.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void error(char* msg){
+    printf("%s\n", msg);
+    printf("%s\n", strerror(errno));
+    exit(EXIT_FAILURE);
+}
+
+#endif
