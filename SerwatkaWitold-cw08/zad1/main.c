ce4648ac3d4dba02c7b3f9b5b7ffc488abcc3682
wsrtka <witold.serwatka@pm.me>
2020-05-04 09:58:46 +0200

    added file header reading

--- SerwatkaWitold-cw08/zad1/main.c
+++ SerwatkaWitold-cw08/zad1/main.c
@@ -11,6 +11,45 @@ void error(char* msg){
     exit(EXIT_FAILURE);
 }
 
+void read_file_header(FILE* file, int* width, int* height, int* maxval){
+    char* line = NULL;
+    char* value = NULL;
+
+    for(int i = 0; i < 4; i++){
+        if(getline(&line, NULL, file) == -1){
+            error("Could not read header file.");
+        }
+
+        switch (i)
+        {
+        case 0:
+            if(strcmp(line, "P2") != 0){
+                error("Invalid file header.");
+            }
+            break;
+
+        case 1:
+            value = strtok(line, " ");
+            value = strtok(NULL, " ");
+            printf("Reading file %s.\n", value);
+            break;
+
+        case 2:
+            value = strtok(line, " ");
+
+            value = strtok(NULL, " ");
+            break;
+
+        case 3:
+            
+            break;
+        
+        default:
+            break;
+        }
+    }
+}
+
 //==========SIGN MODE============//
 
 //==========BLOCK MODE============//
@@ -36,7 +75,8 @@ int main(int argc, char* argv[]){
         error("Could not open input file.");
     }
 
-    
+    int temp_width, temp_height, temp_maxval;
+    read_file_header(input_file, &temp_width, &temp_height, &temp_maxval);
 
     return 0;
 }
