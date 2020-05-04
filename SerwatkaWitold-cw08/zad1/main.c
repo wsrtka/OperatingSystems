b2d258a76a2add77003e973d0a91476e6388ab80
wsrtka <witold.serwatka@pm.me>
2020-05-04 10:29:07 +0200

    added image reading

--- SerwatkaWitold-cw08/zad1/main.c
+++ SerwatkaWitold-cw08/zad1/main.c
@@ -12,6 +12,8 @@ void error(char* msg){
 }
 
 void read_file_header(FILE* file, int* width, int* height, int* maxval){
+    rewind(file);
+
     char* line = NULL;
     char* value = NULL;
 
@@ -36,17 +38,41 @@ void read_file_header(FILE* file, int* width, int* height, int* maxval){
 
         case 2:
             value = strtok(line, " ");
-
+            *width = atoi(value);
             value = strtok(NULL, " ");
+            *height = atoi(value);
             break;
 
         case 3:
-            
+            *maxval = atoi(line);
             break;
         
         default:
             break;
         }
+    }
+}
+
+void read_image(FILE* file, int width, int heigth, int image[width][heigth]){
+    char* line = NULL;
+    char* value = NULL;
+    int i = 0, j = 0;
+
+    while(getline(&line, NULL, file) != 0){
+        value = strtok(line, " ");
+        do{
+            image[i++][j] = atoi(value);
+
+            if(i == width){
+                i = 0;
+                j++;
+            }
+
+            if(j == heigth){
+                printf("Invalid image dimensions, trimming image.\n");
+                return;
+            }
+        } while((value = strtok(NULL, " ")) != NULL);
     }
 }
 
@@ -77,6 +103,15 @@ int main(int argc, char* argv[]){
 
     int temp_width, temp_height, temp_maxval;
     read_file_header(input_file, &temp_width, &temp_height, &temp_maxval);
+    const int width = temp_width;
+    const int height = temp_height;
+    const int maxval = temp_maxval;
+    if(width < 1 || height < 1 || maxval < 0){
+        error("Invalid image metadata.");
+    }
+
+    int image[width][height];
+    read_image(input_file, width, height, image);
 
     return 0;
 }
