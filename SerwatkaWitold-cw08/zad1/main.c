540a153fde1cafca79548dbf7903831a2f906d9f
wsrtka <witold.serwatka@pm.me>
2020-05-04 16:11:06 +0200

    added time measurements

--- SerwatkaWitold-cw08/zad1/main.c
+++ SerwatkaWitold-cw08/zad1/main.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+#include <sys/time.h>
 
 int* histogram;
 
@@ -89,8 +90,47 @@ void read_image(FILE* file, int width, int heigth, int** image){
     }
 }
 
+struct timeval* time_diff(struct timeval tv1, struct timeval tv2){
+    struct timeval* result = malloc(sizeof(struct timeval));
+
+    result->tv_sec = tv1.tv_sec - tv2.tv_sec;
+    result->tv_usec = tv1.tv_usec - tv2.tv_usec;
+
+    if(result->tv_usec < 0){
+        result->tv_sec--;
+        result->tv_usec = -result->tv_usec;
+    }
+
+    return result;
+}
+
+void print_time(struct timeval* tv_ptr){
+    printf("%ld.%ld seconds\n", tv_ptr->tv_sec, tv_ptr->tv_usec);
+}
+
+void fprint_time(struct timeval* tv_ptr, FILE* file){
+    fprintf(file, "%ld.%ld seconds\n", tv_ptr->tv_sec, tv_ptr->tv_usec);
+}
+
+void save_histogram(const char* file_name){
+    FILE* result = fopen(file_name, "w");
+    if(result == NULL){
+        error("Could not create histogram file.");
+    }
+
+    for(int i = 0; i < 255; i++){
+        fprintf(result, "%d\n", histogram[i]);
+    }
+    fprintf(result, "%d", histogram[255]);
+
+    fclose(result);
+}
+
 //==========SIGN MODE============//
 void* sign_function(void* arg){
+    struct timeval thread_start;
+    gettimeofday(&thread_start, NULL);
+
     thread_args* args = (thread_args*) arg;
 
     for(int i = 0; i < args->img_width; i++){
@@ -105,7 +145,12 @@ void* sign_function(void* arg){
 
     }
 
-    return NULL;
+    struct timeval thread_end;
+    gettimeofday(&thread_end, NULL);
+
+    struct timeval* result = time_diff(thread_end, thread_start);
+
+    pthread_exit(result);
 }
 
 void thread_sign(const int thread_number, thread_args* args, pthread_t* threads){
@@ -136,10 +181,10 @@ void thread_sign(const int thread_number, thread_args* args, pthread_t* threads)
 
 //==========BLOCK MODE============//
 void* block_function(void* arg){
-    thread_args* args = (thread_args*) arg;
+    struct timeval thread_start;
+    gettimeofday(&thread_start, NULL);
 
-    printf("BLOCKIODSAD.\n");
-    printf("%d, %d\n", args->from, args->to);
+    thread_args* args = (thread_args*) arg;
 
     for(int i = args->from; i < args->to; i++){
 
@@ -149,7 +194,12 @@ void* block_function(void* arg){
 
     }
 
-    return NULL;
+    struct timeval thread_end;
+    gettimeofday(&thread_end, NULL);
+
+    struct timeval* result = time_diff(thread_end, thread_start);
+
+    pthread_exit(result);
 }
 
 void thread_block(const int thread_number, thread_args* args, pthread_t* threads){
@@ -162,8 +212,6 @@ void thread_block(const int thread_number, thread_args* args, pthread_t* threads
 
         final[i]->from = i * range;
         final[i]->to = (i+1) * range;
-
-        printf("%d, %d\n", final[i]->from, final[i]->to);
 
         if(i == thread_number - 1 && final[i]->to <= args->img_width){
             final[i]->to = args->img_width;
@@ -182,46 +230,43 @@ void thread_block(const int thread_number, thread_args* args, pthread_t* threads
 
 //==========INTERLEAVED MODE============//
 void* interleaved_function(void* arg){
+    struct timeval thread_start;
+    gettimeofday(&thread_start, NULL);
+
     thread_args* args = (thread_args*) arg;
 
-    printf("%d, %d\n", args->from, args->to);
-
-    for(int i = 0; i < args->img_width; i++){
+    for(int i = args->from; i < args->img_width; i += args->to){
 
         for(int j = 0; j < args->img_heigth; j++){
 
-            if(args->img[i][j] >= args->from && args->img[i][j] < args->to){
-                args->histogram[args->img[i][j]]++;
-            }
+            args->histogram[args->img[i][j]]++;
 
         }
 
     }
 
-    return NULL;
+    struct timeval thread_end;
+    gettimeofday(&thread_end, NULL);
+
+    struct timeval* result = time_diff(thread_end, thread_start);
+
+    pthread_exit(result);
 }
 
 void thread_interleaved(const int thread_number, thread_args* args, pthread_t* threads){
-    int range = 256 / thread_number;
-
     thread_args* final[thread_number];
 
     for(int i = 0; i < thread_number; i++){
         final[i] = malloc(sizeof(thread_args));
 
-        final[i]->from = i * range;
-        final[i]->to = (i+1) * range;
-
-        if(i == thread_number - 1 && args->to < 256){
-            final[i]->to = 256;
-        }
-
+        final[i]->from = i;
+        final[i]->to = thread_number;
         final[i]->histogram = args->histogram;
         final[i]->img = args->img;
         final[i]->img_heigth = args->img_heigth;
         final[i]->img_width = args->img_width;
 
-        if(pthread_create(&threads[i], NULL, sign_function, (void *) final[i]) != 0){
+        if(pthread_create(&threads[i], NULL, interleaved_function, (void *) final[i]) != 0){
             error("Could not create thread.");
         }
     }
@@ -246,19 +291,15 @@ int main(int argc, char* argv[]){
         error("Could not open input file.");
     }
 
-    int temp_width, temp_height, temp_maxval;
+    int width, height, maxval;
 
-    read_file_header(input_file, &temp_width, &temp_height, &temp_maxval);
-
-    const int width = temp_width;
-    const int height = temp_height;
-    const int maxval = temp_maxval;
+    read_file_header(input_file, &width, &height, &maxval);
 
     if(width < 1 || height < 1 || maxval < 0){
         error("Invalid image metadata.");
     }
 
-    histogram = calloc(256, sizeof(int));
+    histogram = calloc(maxval + 1, sizeof(int));
 
     //ustawianie argumentów dla funkcji threads
     thread_args* args = malloc(sizeof(args));
@@ -273,8 +314,13 @@ int main(int argc, char* argv[]){
 
     read_image(input_file, width, height, args->img);
 
+    fclose(input_file);
+
     //wątkowanie
     pthread_t threads[THREADS_NO];
+    
+    struct timeval main_start;
+    gettimeofday(&main_start, NULL);
 
     if(strcmp(MODE, "sign") == 0){
         thread_sign(THREADS_NO, args, threads);
@@ -290,15 +336,30 @@ int main(int argc, char* argv[]){
     }
 
     //podsumowanie
+    void* retrval;
+    FILE* times_file = fopen("Times.txt", "a");
+    if(times_file == NULL){
+        error("Could not open Times.txt.");
+    }
+
     for(int i = 0; i < THREADS_NO; i++){
-        if(pthread_join(threads[i], NULL) == 0){
-            printf("Thread %d joined.\n", i);
+        if(pthread_join(threads[i], &retrval) == 0){
+            printf("Thread %ld worked for ", threads[i]);
+            print_time(retrval);
+            fprintf(times_file, "Thread %ld worked for ", threads[i]);
+            fprint_time(retrval, times_file);
         }
     }
 
-    for(int i = 0; i < 256; i++){
-        printf("%d\n", histogram[i]);
-    }
+    struct timeval main_end;
+    gettimeofday(&main_end, NULL);
+
+    printf("Main function finished after ");
+    print_time(time_diff(main_end, main_start));
+    fprintf(times_file, "Main function finished after ");
+    fprint_time(time_diff(main_end, main_start), times_file);
+
+    save_histogram(OUT_FILE);
 
     return 0;
 }
