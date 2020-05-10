c2df193f88d7e77bb054724c0cbdab90e3cfbb80
wsrtka <witold.serwatka@pm.me>
2020-05-10 12:10:52 +0200

    basic main function

--- SerwatkaWitold-cw09/zad1/main.c
+++ SerwatkaWitold-cw09/zad1/main.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <errno.h>
 #include <pthread.h>
+#include <time.h>
 
 void error(char* msg){
     printf("Error: %s\n", msg);
@@ -26,5 +27,39 @@ int main(int argc, char* argv[]){
     const int CHAIRS = atoi(argv[1]);
     const int CUSTOMERS = atoi(argv[2]);
 
-    
+    pthread_t barber;
+    pthread_t customer[CUSTOMERS];
+
+    if(pthread_create(&barber, NULL, barber, NULL) != 0){
+        error("Could not initialize barber.");
+    }
+
+    srand(time(NULL));
+
+    for(int i = 0; i < CUSTOMERS; i++){
+        sleep((rand() % 10) + 1);
+
+        if(pthread_create(&customer[i], NULL, customer, NULL) != 0){
+            error("Could not create customer.");
+        }
+    }
+
+    void* retval;
+    for(int i = 0; i < CUSTOMERS; i++){
+        if(pthread_join(customer[i], &retval) != 0){
+            printf("Could not join thread no %d.\n", i);
+        }
+        else{
+            printf("Thread no %d joined.\n", i);
+        }
+    }
+
+    if(pthread_join(barber, &retval) != 0){
+        printf("Could not join barber thread.\n");
+    }
+    else{
+        printf("Barber thread joined.\n");
+    }
+
+    return 0;
 }
