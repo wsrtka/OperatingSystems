ea5fa49d61985ca35c9a5bc7ae5406d973cae93f
wsrtka <witold.serwatka@pm.me>
2020-05-18 12:55:01 +0200

    added server setup for unix socket

--- SerwatkaWitold-cw10/zad1/common.h
+++ SerwatkaWitold-cw10/zad1/common.h
@@ -1,15 +1,32 @@
 #ifndef common
 #define common
 
+#define MAX_CLIENTS 16
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
+#include <signal.h>
+#include <pthread.h>
+
+#include <netinet/in.h>
+
+#include <sys/un.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+typedef struct client{
+    int socket_fd;
+    int registered;
+    int active;
+    char* name;
+} client;
 
 //helper functions
 void error(char* msg){
     printf("%s\n", msg);
-    printf("%s", strerror(errno));
+    printf("%s\n", strerror(errno));
     exit(EXIT_FAILURE);
 }
 
--- SerwatkaWitold-cw10/zad1/server.c
+++ SerwatkaWitold-cw10/zad1/server.c
@@ -1,3 +1,57 @@
 #include "common.h"
 
-int main
+in_port_t inet_port;
+char* socket_path;
+int socket_fd;
+
+client clients[MAX_CLIENTS];
+
+pthread_mutex_t mut_clients = PTHREAD_MUTEX_INITIALIZER;
+
+void setup_server(in_port_t port, char* path, int* socket_fd){
+
+    struct sockaddr_un addr;
+    strcpy(addr.sun_path, path);
+    addr.sun_family = AF_UNIX;
+
+    if((*socket_fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1){
+        error("Could not create unix socket.");
+    }
+
+    if(bind(*socket_fd, (struct sockaddr*) &addr, sizeof(struct sockaddr_un)) != 0){
+        error("Could not bind socket.");
+    }
+
+    if(listen(*socket_fd, MAX_CLIENTS) != 0){
+        error("Could not start accepting client connection requests.");
+    }
+
+}
+
+void close_server(){
+
+}
+
+int main(int argc, char* argv[]){
+    
+    atexit(close_server);
+    signal(SIGINT, close_server);
+
+    if(argc != 3){
+        error("Invalid number of arguments.");
+    }
+
+    inet_port = (in_port_t) atoi(argv[1]);
+    socket_path = argv[2];
+
+    for(int i = 0; i < MAX_CLIENTS; i++){
+        clients[i].active = 0;
+        clients[i].name = "\0";
+        clients[i].registered = 0;
+        clients[i].socket_fd = -1;
+    }
+
+    setup_server(inet_port, socket_path, &socket_fd);
+
+    return 0;
+}
