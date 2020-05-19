16a20749d7ee6554d4ec76ff856b5c030e1baea0
wsrtka <witold.serwatka@pm.me>
2020-05-19 11:10:36 +0200

    added connection manager

--- SerwatkaWitold-cw10/zad1/common.h
+++ SerwatkaWitold-cw10/zad1/common.h
@@ -2,6 +2,9 @@
 #define common
 
 #define MAX_CLIENTS 16
+#define NAME_LEN 64
+#define PING_INTERVAL 10
+#define PING_TIMEOUT 5
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -9,12 +12,14 @@
 #include <errno.h>
 #include <signal.h>
 #include <pthread.h>
+#include <unistd.h>
 
 #include <netinet/in.h>
 
 #include <sys/un.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/epoll.h>
 
 typedef struct client{
     int socket_fd;
--- SerwatkaWitold-cw10/zad1/server.c
+++ SerwatkaWitold-cw10/zad1/server.c
@@ -8,6 +8,8 @@ client clients[MAX_CLIENTS];
 
 pthread_mutex_t mut_clients = PTHREAD_MUTEX_INITIALIZER;
 
+
+//===========SERVER FUNCTIONS=============//
 void setup_server(in_port_t port, char* path, int* socket_fd){
 
     struct sockaddr_un addr;
@@ -40,6 +42,64 @@ void close_server(){
     }
 }
 
+//===========THREAD FUNCTIONS==============//
+void* connection_manager_f(void* args){
+
+    while(1){
+
+        struct sockaddr_un client_addr;
+        socklen_t client_addr_len = sizeof(client_addr);
+
+        int i, new_client_fd = accept(socket_fd, (struct sockaddr *) &client_addr, &client_addr_len);
+
+        pthread_mutex_lock(&mut_clients);
+
+        for(i = 0; i < MAX_CLIENTS; i++){
+            if(clients[i].socket_fd == -1){
+                clients[i].socket_fd = new_client_fd;
+                clients[i].registered = 1;
+
+                write(new_client_fd, "Client registered.", 18);
+                read(new_client_fd, clients[i].name, NAME_LEN);
+            }
+        }
+
+        if(i == MAX_CLIENTS){
+            printf("Could not register new client, already reached max client number.\n");
+        }
+
+        pthread_mutex_unlock(&mut_clients);
+
+    }
+
+    return 0;
+}
+
+void* ping_manager_f(void* args){
+
+    while(1){
+
+        sleep(3);
+
+        pthread_mutex_lock(&mut_clients);
+
+        for(int i = 0; i < MAX_CLIENTS; i++){
+
+            if(clients[i].registered == 1){
+                
+            }
+
+        }
+
+    }
+
+    return 0;
+}
+
+void* game_manager_f(void* args){
+    return 0;
+}
+
 int main(int argc, char* argv[]){
     
     atexit(close_server);
@@ -54,12 +114,24 @@ int main(int argc, char* argv[]){
 
     for(int i = 0; i < MAX_CLIENTS; i++){
         clients[i].active = 0;
-        clients[i].name = "\0";
+        bzero((char*) clients[i].name, sizeof(clients[i].name));
         clients[i].registered = 0;
         clients[i].socket_fd = -1;
     }
 
     setup_server(inet_port, socket_path, &socket_fd);
+
+    pthread_t connection_manager;
+    pthread_t ping_manager;
+    pthread_t game_manager;
+
+    pthread_create(&connection_manager, NULL, connection_manager_f, NULL);
+    pthread_create(&ping_manager, NULL, ping_manager_f, NULL);
+    pthread_create(&game_manager, NULL, game_manager_f, NULL);
+
+    pthread_join(connection_manager, NULL);
+    pthread_join(ping_manager, NULL);
+    pthread_join(game_manager, NULL);
 
     return 0;
 }
