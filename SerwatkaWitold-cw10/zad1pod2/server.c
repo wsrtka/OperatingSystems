b2e542940354181286012bbd7c95b5ba6a767039
wsrtka <witold.serwatka@pm.me>
2020-05-24 12:57:53 +0200

    added main function skeleton

--- SerwatkaWitold-cw10/zad1pod2/server.c
+++ SerwatkaWitold-cw10/zad1pod2/server.c
@@ -7,6 +7,12 @@ int main(int argc, char* argv[]){
     atexit(close_server);
     signal(SIGINT, close_server);
 
+    if(argc != 3){
+        
+        error("Invalid number of arguments.");
+
+    }
+
     close_server();
 
     return 0;
