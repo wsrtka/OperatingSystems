3cf7a6fe081ebd25d9299aaa38bbc3612ea26a6d
wsrtka <witold.serwatka@pm.me>
2020-05-24 12:49:26 +0200

    added basic libraries and error function

--- SerwatkaWitold-cw10/zad1pod2/common.h
+++ SerwatkaWitold-cw10/zad1pod2/common.h
@@ -1,6 +1,22 @@
 #ifndef common
 #define common
 
+//standard libs
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+//libs needed for socket operations
 #include <sys/socket.h>
+
+//libs for threading
+#include <pthread.h>
+
+void error(char* msg){
+    printf("%s\n", msg);
+    printf("%s\n", strerror(errno));
+    exit(EXIT_FAILURE);
+}
 
 #endif
