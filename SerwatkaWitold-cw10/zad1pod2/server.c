8067a32f58742de1dde28efe61eb30875adf2bb2
wsrtka <witold.serwatka@pm.me>
2020-05-24 12:58:42 +0200

    added main function skeleton

--- SerwatkaWitold-cw10/zad1pod2/server.c
+++ SerwatkaWitold-cw10/zad1pod2/server.c
@@ -1,19 +1,40 @@
 #include "common.h"
 
+
+//global args
+
+
+void open_server();
+
 void close_server();
+
+
+void* ping_manager_f(void* args);
+
 
 int main(int argc, char* argv[]){
 
     atexit(close_server);
     signal(SIGINT, close_server);
 
-    if(argc != 3){
-        
-        error("Invalid number of arguments.");
 
+    if(argc != 3){
+        error("Invalid number of arguments.");
     }
 
+    //get command line args
+
+
+    open_server();
+
+
+    pthread_t ping_manager;
+
+    pthread_create(ping_manager, NULL, ping_manager_f, NULL);
+
+
     close_server();
+
 
     return 0;
 
